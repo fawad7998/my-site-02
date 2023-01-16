15a6ed783450d49d9798c1e46591b7d312de9c9a
FAWAd7998 <fawad7998@gmail.com>
2023-01-17 01:55:03 +0500

    second commit

---
+++ my_website.js
@@ -0,0 +1,25 @@
+function setRoot() {
+    let array = ["free", "standard", "enterprise"];
+    let root = document.getElementById("root");
+    let badgeStyle = `
+    // display: flex;
+	// justify-content: center;
+    // margin-left: 20px;
+    margin-bottom: 20px;
+    height: 300px;
+    width: 320px;
+    background-color: white;
+    box-shadow: 5px 5px 20px #888888;
+    `
+    for (let index = 0; index < array.length; index++) {
+        let badge = document.createElement("div");
+        badge.style.cssText = badgeStyle
+        // badge.style.display = flex;
+        // badge.id.add('box-01')
+        badge.innerHTML = array[index];
+        root.append(badge);
+    }
+}
+
+// aO = [{title:"", text:""}, {title:"", text:""}, {title:"", text:""}, {title:"", text:""}]
+setRoot();
