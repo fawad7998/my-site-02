b4d8e1c7dd6d69dcb323b5e93509f2e3d11b2f89
FAWAd7998 <fawad7998@gmail.com>
2023-01-17 02:00:10 +0500

    nothing

--- my_website.js
+++ my_website.js
@@ -1,7 +1,7 @@
 function setRoot() {
-    let array = ["free", "standard", "enterprise"];
-    let root = document.getElementById("root");
-    let badgeStyle = `
+	let array = ["free", "standard", "enterprise"];
+	let root = document.getElementById("root");
+	let badgeStyle = `
     // display: flex;
 	// justify-content: center;
     // margin-left: 20px;
@@ -10,16 +10,24 @@ function setRoot() {
     width: 320px;
     background-color: white;
     box-shadow: 5px 5px 20px #888888;
-    `
-    for (let index = 0; index < array.length; index++) {
-        let badge = document.createElement("div");
-        badge.style.cssText = badgeStyle
-        // badge.style.display = flex;
-        // badge.id.add('box-01')
-        badge.innerHTML = array[index];
-        root.append(badge);
-    }
+    `;
+	for (let index = 0; index < array.length; index++) {
+		let badge = document.createElement("div");
+		badge.style.cssText = badgeStyle;
+		// badge.style.display = flex;
+		// badge.id.add('box-01')
+		badge.innerHTML = array[index];
+		root.append(badge);
+	}
 }
 
 // aO = [{title:"", text:""}, {title:"", text:""}, {title:"", text:""}, {title:"", text:""}]
 setRoot();
+
+
+
+
+//  can it creates a div in <div id[root]>
+// display flex prop 
+// can we pick css id rather then class then how 
+// how we raplicte any div or section 
