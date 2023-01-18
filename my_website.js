2b2a582c43fc22d95f29c5be3b3e0b5f1062527d
FAWAd7998 <fawad7998@gmail.com>
2023-01-18 05:44:26 +0500

    commit

--- my_website.js
+++ my_website.js
@@ -1,33 +1,66 @@
+import styles from "./styles.js";
+
 function setRoot() {
-	let array = ["free", "standard", "enterprise"];
+	console.log(styles);
+    
+    let array = [
+        {
+            title: "free",
+            packageDetails: [
+                "1 User",
+                "Community Support",
+                "Style Customizer",
+                "Expanded Utilities",
+                "Third Party Integration",
+                "Layout Options",
+            ],
+        },
+        {
+            title: "standard",
+            packageDetails: [
+                "1 User",
+                "Community Support",
+                "Style Customizer",
+                "Expanded Utilities",
+                "Third Party Integration",
+                "Layout Options",
+            ],
+        },
+        {
+            title: "enterprise",
+            packageDetails: [
+                "1 User",
+                "Community Support",
+                "Style Customizer",
+                "Expanded Utilities",
+                "Third Party Integration",
+                "Layout Options",
+            ],
+        },
+    ];
 	let root = document.getElementById("root");
-	let badgeStyle = `
-    // display: flex;
-	// justify-content: center;
-    // margin-left: 20px;
-    margin-bottom: 20px;
-    height: 300px;
-    width: 320px;
-    background-color: white;
-    box-shadow: 5px 5px 20px #888888;
-    `;
+	root.style.display = "flex";
 	for (let index = 0; index < array.length; index++) {
 		let badge = document.createElement("div");
-		badge.style.cssText = badgeStyle;
-		// badge.style.display = flex;
-		// badge.id.add('box-01')
-		badge.innerHTML = array[index];
+		badge.style.cssText = styles.badgeStyle;
+		badge.setAttribute("id", "box-01");
+		badge.innerHTML = array[index].title;
+        let packageDetails = document.createElement("div");
+        packageDetails.classList.add('package-details')
+        let packageDetailsTexts = array[index].packageDetails
+		for (let index = 0; index < packageDetailsTexts.length; index++) {
+            const text = packageDetailsTexts[index];
+            let packageDetailsTextDiv = document.createElement("div");
+            packageDetailsTextDiv.innerText = text
+            packageDetails.append(packageDetailsTextDiv);
+        }   
+        badge.insertBefore(packageDetails, badge)
 		root.append(badge);
 	}
 }
-
-// aO = [{title:"", text:""}, {title:"", text:""}, {title:"", text:""}, {title:"", text:""}]
 setRoot();
 
-
-
-
 //  can it creates a div in <div id[root]>
-// display flex prop 
-// can we pick css id rather then class then how 
-// how we raplicte any div or section 
+// display flex prop
+// can we pick css id rather then class then how
+// how we raplicte any root or section
---
+++ styles.js
@@ -0,0 +1,14 @@
+const styles = {
+	badgeStyle: `
+        // display: flex;
+        // justify-content: center;
+        // margin-left: 20px;
+        margin-bottom: 20px;
+        height: 300px;
+        width: 320px;
+        background-color: white;
+        box-shadow: 5px 5px 20px #888888; 
+    `,
+
+};
+export default styles;
